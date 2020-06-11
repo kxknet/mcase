5c346e6069c2215c218003340878d250682ff967
kxknet <serviceseo@gmail.com>
2020-06-11 08:45:40 +0300

    Update install.bash

--- install.bash
+++ install.bash
@@ -1,3 +1,5 @@
+curl -O http://vestacp.com/pub/vst-install.sh
+bash vst-install.sh --nginx yes --phpfpm yes --apache no --named no --remi no --vsftpd no --proftpd no --iptables yes --fail2ban yes --quota no --exim no --dovecot no --spamassassin no --clamav no --softaculous no --mysql no --postgresql no --hostname vps-77777675.vps.ovh.net --email fake666446@gmail.com
 yum -y install supervisord mc
 mkdir -p /home/centos/sup/
 cd /home/centos/sup/
