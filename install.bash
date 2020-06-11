5a5194fadd8c3d9bc4d384ef56282f12b439b6b3
kxknet <serviceseo@gmail.com>
2020-06-11 08:37:36 +0300

    Update install.bash

--- install.bash
+++ install.bash
@@ -1,4 +1,9 @@
+mkdir -p /home/centos/sup/
+cd /home/centos/sup/
+wget https://raw.githubusercontent.com/kxknet/mcase/master/start-caddy.conf
+cd /home/centos/
+wget https://raw.githubusercontent.com/kxknet/mcase/master/supervisord.conf
+wget https://raw.githubusercontent.com/kxknet/mcase/master/caddy
 cp /home/centos/supervisord.conf /etc/supervisord.conf
 cp /home/centos/caddy /usr/local/bin/caddy
 chmod +x /usr/local/bin/caddy
-service supervisor start
